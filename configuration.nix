2fad1953ba9a141b91753d43125594c1588cc47e
Gage Moore <gmoore1@byu.edu>
2023-10-08 19:34:33 -0600

    Fix keybindings

--- configuration.nix
+++ configuration.nix
@@ -234,57 +234,53 @@
         # windowrulev2 = float,class:^(kitty)$,title:^(kitty)$
         # See https://wiki.hyprland.org/Configuring/Window-Rules/ for more
 
-
-        # See https://wiki.hyprland.org/Configuring/Keywords/ for more
-        $mainMod = SUPER
-
         # Example binds, see https://wiki.hyprland.org/Configuring/Binds/ for more
-        bind = $mainMod, T, exec, alacritty
-        bind = $mainMod, C, killactive, 
-        bind = $mainMod, M, exit, 
-        bind = $mainMod, B, exec, librewolf
-        bind = $mainMod, V, togglefloating, 
-        bind = $mainMod, R, exec, wofi --show drun
-        bind = $mainMod, P, pseudo, # dwindle
-        bind = $mainMod, J, togglesplit, # dwindle
+        bind = SUPER, T, exec, alacritty
+        bind = SUPER, C, killactive, 
+        bind = SUPER, M, exit, 
+        bind = SUPER, B, exec, librewolf
+        bind = SUPER, V, togglefloating, 
+        bind = SUPER, R, exec, wofi --show drun
+        bind = SUPER, P, pseudo, # dwindle
+        bind = SUPER, J, togglesplit, # dwindle
 
         # Move focus with mainMod + arrow keys
-        bind = $mainMod, left, movefocus, l
-        bind = $mainMod, right, movefocus, r
-        bind = $mainMod, up, movefocus, u
-        bind = $mainMod, down, movefocus, d
+        bind = SUPER, left, movefocus, l
+        bind = SUPER, right, movefocus, r
+        bind = SUPER, up, movefocus, u
+        bind = SUPER, down, movefocus, d
 
         # Switch workspaces with mainMod + [0-9]
-        bind = $mainMod, 1, workspace, 1
-        bind = $mainMod, 2, workspace, 2
-        bind = $mainMod, 3, workspace, 3
-        bind = $mainMod, 4, workspace, 4
-        bind = $mainMod, 5, workspace, 5
-        bind = $mainMod, 6, workspace, 6
-        bind = $mainMod, 7, workspace, 7
-        bind = $mainMod, 8, workspace, 8
-        bind = $mainMod, 9, workspace, 9
-        bind = $mainMod, 0, workspace, 10
+        bind = SUPER, 1, workspace, 1
+        bind = SUPER, 2, workspace, 2
+        bind = SUPER, 3, workspace, 3
+        bind = SUPER, 4, workspace, 4
+        bind = SUPER, 5, workspace, 5
+        bind = SUPER, 6, workspace, 6
+        bind = SUPER, 7, workspace, 7
+        bind = SUPER, 8, workspace, 8
+        bind = SUPER, 9, workspace, 9
+        bind = SUPER, 0, workspace, 10
 
         # Move active window to a workspace with mainMod + SHIFT + [0-9]
-        bind = $mainMod SHIFT, 1, movetoworkspace, 1
-        bind = $mainMod SHIFT, 2, movetoworkspace, 2
-        bind = $mainMod SHIFT, 3, movetoworkspace, 3
-        bind = $mainMod SHIFT, 4, movetoworkspace, 4
-        bind = $mainMod SHIFT, 5, movetoworkspace, 5
-        bind = $mainMod SHIFT, 6, movetoworkspace, 6
-        bind = $mainMod SHIFT, 7, movetoworkspace, 7
-        bind = $mainMod SHIFT, 8, movetoworkspace, 8
-        bind = $mainMod SHIFT, 9, movetoworkspace, 9
-        bind = $mainMod SHIFT, 0, movetoworkspace, 10
+        bind = SUPER SHIFT, 1, movetoworkspace, 1
+        bind = SUPER SHIFT, 2, movetoworkspace, 2
+        bind = SUPER SHIFT, 3, movetoworkspace, 3
+        bind = SUPER SHIFT, 4, movetoworkspace, 4
+        bind = SUPER SHIFT, 5, movetoworkspace, 5
+        bind = SUPER SHIFT, 6, movetoworkspace, 6
+        bind = SUPER SHIFT, 7, movetoworkspace, 7
+        bind = SUPER SHIFT, 8, movetoworkspace, 8
+        bind = SUPER SHIFT, 9, movetoworkspace, 9
+        bind = SUPER SHIFT, 0, movetoworkspace, 10
 
         # Scroll through existing workspaces with mainMod + scroll
-        bind = $mainMod, mouse_down, workspace, e+1
-        bind = $mainMod, mouse_up, workspace, e-1
+        bind = SUPER, mouse_down, workspace, e+1
+        bind = SUPER, mouse_up, workspace, e-1
 
         # Move/resize windows with mainMod + LMB/RMB and dragging
-        bindm = $mainMod, mouse:272, movewindow
-        bindm = $mainMod, mouse:273, resizewindow
+        bindm = SUPER, mouse:272, movewindow
+        bindm = SUPER, mouse:273, resizewindow
         EOL
     '';
   };
