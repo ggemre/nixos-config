f5a0092e85cc1c81559e117b7271a045caaa8304
ggemre <gmoore1@byu.edu>
2023-10-10 21:14:38 -0600

    Configure waybar

--- configuration.nix
+++ configuration.nix
@@ -61,23 +61,32 @@
 
   # List packages installed in system profile. To search, run:
   environment.systemPackages = with pkgs; [
-    neovim
     curl
     wayland
-    waybar
     wofi
+    pure-prompt
   ];
 
   programs.zsh = {
     enable = true;
-    syntaxHighlighting.enable = true;
     enableCompletion = true;
-    ohMyZsh.enable = true;
-    ohMyZsh.theme = "gentoo";
+    syntaxHighlighting.enable = true;
+    shellAliases = {
+      sudo = "sudo ";
+      vim = "nvim";
+    };
+    promptInit = ''
+      autoload -U promptinit; promptinit
+      prompt pure
+    '';
   };
   users.defaultUserShell = pkgs.zsh;
+  environment.shells = with pkgs; [ zsh ];
 
-  programs.hyprland.enable = true;
+  programs.hyprland = {
+    enable = true;
+    xwayland.enable = true;
+  };
   services.greetd = {
     enable = true;
     settings = {
@@ -88,7 +97,7 @@
     };
   };
 
-  fonts.fonts = with pkgs; [
+  fonts.packages = with pkgs; [
     noto-fonts
     noto-fonts-cjk
     noto-fonts-emoji
@@ -130,6 +139,10 @@
       userName = "ggemre";
       userEmail = "gmoore1@byu.edu";
     };
+    programs.neovim = {
+      enable = true;
+      defaultEditor = true;
+    };
     programs.alacritty = {
       enable = true;
       settings = {
@@ -159,19 +172,66 @@
 	};
       };
     };
+    programs.waybar = {
+      enable = true;
+      settings = {
+        main = {
+	  layer = "top";
+	  position = "top";
+	  height = 30;
+	  "modules-left" = [
+	    "hyprland/workspaces"
+	  ];
+	  "modules-center" = [ "clock" ];
+	  "modules-right" = [
+	    "battery"
+	    "pulseaudio"
+	  ];
+	  "hyprland/workspaces" = {
+	    format = "{name}";
+	  };
+	  clock = {
+	    interval = 1;
+	    format = "{:%I:%M %p}";
+	    tooltip = false;
+	  };
+	  battery = {
+	    interval = 10;
+	    states = {
+	      warning = 20;
+	      critical = 10;
+            };
+	    format = "{capacity}%";
+	    tooltip = false;
+	  };
+	  pulseaudio = {
+	    "scroll-step" = 1;
+	    format = "{volume}%";
+	    tooltip = false;
+	  };
+	};
+      };
+      style = ''
+        * {
+	  border: none;
+	  border-radius: 8;
+	  font-family: Fira Code;
+        }
+      '';
+    };
     home.file.".config/hypr/hyprland.conf".text = ''
 	# See https://wiki.hyprland.org/Configuring/Monitors/
 	monitor=,preferred,auto,auto
-	
+
 	# Execute your favorite apps at launch
-	exec-once = waybar & # hyprpaper & firefox
-	
+	exec-once = waybar # & hyprpaper & firefox
+
 	# Source a file (multi-file configs)
 	# source = ~/.config/hypr/myColors.conf
-	
+
 	# Some default env vars.
 	env = XCURSOR_SIZE,24
-	
+
 	# For all categories, see https://wiki.hyprland.org/Configuring/Variables/
 	input {
 	    kb_layout = us
@@ -179,55 +239,52 @@
 	    kb_model =
 	    kb_options =
 	    kb_rules =
-	
+
 	    follow_mouse = 1
-	
+
 	    touchpad {
-	        natural_scroll = false
+		natural_scroll = no
 	    }
-	
+
 	    sensitivity = 0 # -1.0 - 1.0, 0 means no modification.
 	}
-	
+
 	general {
 	    # See https://wiki.hyprland.org/Configuring/Variables/ for more
-	
+
 	    gaps_in = 5
 	    gaps_out = 20
 	    border_size = 2
 	    col.active_border = rgba(33ccffee) rgba(00ff99ee) 45deg
 	    col.inactive_border = rgba(595959aa)
-	
+
 	    layout = dwindle
-	
-	    # Please see https://wiki.hyprland.org/Configuring/Tearing/ before you turn this on
-	    allow_tearing = false
 	}
-	
+
 	decoration {
 	    # See https://wiki.hyprland.org/Configuring/Variables/ for more
-	
+
 	    rounding = 10
-	
+	    
 	    blur {
-	        enabled = true
-	        size = 3
-	        passes = 1
+		enabled = true
+		size = 3
+		passes = 1
 	    }
-	
-	    drop_shadow = true
+
+	    drop_shadow = yes
 	    shadow_range = 4
 	    shadow_render_power = 3
 	    col.shadow = rgba(1a1a1aee)
 	}
-	
+
 	animations {
-	    enabled = true
-	
+	    enabled = yes
+
 	    # Some default animations, see https://wiki.hyprland.org/Configuring/Animations/ for more
-	
+
 	    bezier = myBezier, 0.05, 0.9, 0.1, 1.05
-	
+
 	    animation = windows, 1, 7, myBezier
 	    animation = windowsOut, 1, 7, default, popin 80%
 	    animation = border, 1, 10, default
@@ -235,59 +292,54 @@
 	    animation = fade, 1, 7, default
 	    animation = workspaces, 1, 6, default
 	}
-	
+
 	dwindle {
 	    # See https://wiki.hyprland.org/Configuring/Dwindle-Layout/ for more
-	    pseudotile = true # master switch for pseudotiling. Enabling is bound to mainMod + P in the keybinds section below
-	    preserve_split = true # you probably want this
+	    pseudotile = yes # master switch for pseudotiling. Enabling is bound to mainMod + P in the keybinds section below
+	    preserve_split = yes # you probably want this
 	}
-	
+
 	master {
 	    # See https://wiki.hyprland.org/Configuring/Master-Layout/ for more
 	    new_is_master = true
 	}
-	
+
 	gestures {
 	    # See https://wiki.hyprland.org/Configuring/Variables/ for more
-	    workspace_swipe = false
+	    workspace_swipe = off
 	}
-	
-	misc {
-	    # See https://wiki.hyprland.org/Configuring/Variables/ for more
-	    force_default_wallpaper = -1 # Set to 0 to disable the anime mascot wallpapers
-	}
-	
+
 	# Example per-device config
-	# See https://wiki.hyprland.org/Configuring/Keywords/#per-device-input-configs for more
+	# See https://wiki.hyprland.org/Configuring/Keywords/#executing for more
 	device:epic-mouse-v1 {
 	    sensitivity = -0.5
 	}
-	
+
 	# Example windowrule v1
 	# windowrule = float, ^(kitty)$
 	# Example windowrule v2
 	# windowrulev2 = float,class:^(kitty)$,title:^(kitty)$
 	# See https://wiki.hyprland.org/Configuring/Window-Rules/ for more
-	
-	
+
+
 	# See https://wiki.hyprland.org/Configuring/Keywords/ for more
 	$mainMod = SUPER
-	
+
 	# Example binds, see https://wiki.hyprland.org/Configuring/Binds/ for more
 	bind = $mainMod, T, exec, alacritty
-	bind = $mainMod, C, killactive,
-	bind = $mainMod, M, exit,
-	bind = $mainMod, V, togglefloating,
+	bind = $mainMod, C, killactive, 
+	bind = $mainMod, M, exit, 
+	bind = $mainMod, V, togglefloating, 
 	bind = $mainMod, R, exec, wofi --show drun
 	bind = $mainMod, P, pseudo, # dwindle
 	bind = $mainMod, J, togglesplit, # dwindle
-	
+
 	# Move focus with mainMod + arrow keys
 	bind = $mainMod, left, movefocus, l
 	bind = $mainMod, right, movefocus, r
 	bind = $mainMod, up, movefocus, u
 	bind = $mainMod, down, movefocus, d
-	
+
 	# Switch workspaces with mainMod + [0-9]
 	bind = $mainMod, 1, workspace, 1
 	bind = $mainMod, 2, workspace, 2
@@ -299,7 +351,7 @@
 	bind = $mainMod, 8, workspace, 8
 	bind = $mainMod, 9, workspace, 9
 	bind = $mainMod, 0, workspace, 10
-	
+
 	# Move active window to a workspace with mainMod + SHIFT + [0-9]
 	bind = $mainMod SHIFT, 1, movetoworkspace, 1
 	bind = $mainMod SHIFT, 2, movetoworkspace, 2
@@ -311,11 +363,11 @@
 	bind = $mainMod SHIFT, 8, movetoworkspace, 8
 	bind = $mainMod SHIFT, 9, movetoworkspace, 9
 	bind = $mainMod SHIFT, 0, movetoworkspace, 10
-	
+
 	# Scroll through existing workspaces with mainMod + scroll
 	bind = $mainMod, mouse_down, workspace, e+1
 	bind = $mainMod, mouse_up, workspace, e-1
-	
+
 	# Move/resize windows with mainMod + LMB/RMB and dragging
 	bindm = $mainMod, mouse:272, movewindow
 	bindm = $mainMod, mouse:273, resizewindow
