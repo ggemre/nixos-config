3d36d7432d06123881d449b433e7a9550a096c20
ggemre <ggemre+github@proton.me>
2025-08-24 13:26:41 +0700

    feat: fmt no ads patch

--- overlays/patched/default.nix
+++ overlays/patched/default.nix
@@ -1,7 +1,12 @@
 final: prev: {
   alejandra = prev.alejandra.overrideAttrs (old: {
-    patches = (old.patches or []) ++ [ ./alejandra/spaced-elements.patch ];
+    patches =
+      (old.patches or [])
+      ++ [
+        ./alejandra/spaced-elements.patch
+        ./alejandra/no-ads.patch
+      ];
     doCheck = false;
-    meta.description = "Custom Alejandra with spaces around elements.";
+    meta.description = "Custom Alejandra with spaces around elements and 0 ads.";
   });
 }
