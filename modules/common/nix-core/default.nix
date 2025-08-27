c7778ebd1a6eb6a7046a580940da4c8f4ecaa81f
ggemre <ggemre+github@proton.me>
2025-08-27 11:57:45 +0700

    feat: opt-in lix support

--- modules/common/nix-core/default.nix
+++ modules/common/nix-core/default.nix
@@ -1,35 +1,63 @@
 {
+  config,
+  lib,
   pkgs,
   self,
   ...
-}: {
-  nixpkgs = {
-    config = {
-      allowUnfree = true;
-      allowBroken = false;
+}: let
+  cfg = config.common.nix;
+in {
+  options.common.nix = {
+    withLix = lib.mkOption {
+      type = lib.types.bool;
+      default = false;
+      description = "Whether to use the lix package";
     };
   };
 
-  nix = {
-    package = pkgs.nix;
-    channel.enable = false;
-    gc = {
-      automatic = true;
-      options = "--delete-older-than 7d";
+  config = {
+    nixpkgs = {
+      config = {
+        allowUnfree = true;
+        allowBroken = false;
+      };
     };
-    settings = {
-      experimental-features = [ "nix-command" "flakes" ];
-      trusted-users = [ "@wheel" ];
-      auto-optimise-store = true;
-      sandbox = true;
-      substituters = [
-        "https://nix-community.cachix.org"
-        "https://cache.privatevoid.net"
-      ];
-      trusted-public-keys = [
-        "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
-        "cache.privatevoid.net:SErQ8bvNWANeAvtsOESUwVYr2VJynfuc9JRwlzTTkVg="
-      ];
+
+    nix = {
+      package =
+        if cfg.withLix
+        then pkgs.lixPackageSets.latest.nix
+        else pkgs.nix;
+      channel.enable = false;
+      gc = {
+        automatic = true;
+        options = "--delete-older-than 7d";
+      };
+      settings = {
+        experimental-features = [ "nix-command" "flakes" ];
+        trusted-users = [ "@wheel" ];
+        auto-optimise-store = true;
+        sandbox = true;
+        substituters = [
+          "https://nix-community.cachix.org"
+          "https://cache.privatevoid.net"
+        ];
+        trusted-public-keys = [
+          "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+          "cache.privatevoid.net:SErQ8bvNWANeAvtsOESUwVYr2VJynfuc9JRwlzTTkVg="
+        ];
+      };
     };
+
+    # Provide lix package to anyone who needs it
+    programs.direnv.nix-direnv.package =
+      lib.mkIf (
+        config.programs.direnv.enable
+        && config.programs.direnv.nix-direnv.enable
+      ) (
+        if cfg.withLix
+        then pkgs.lixPackageSets.latest.nix-direnv
+        else pkgs.nix-direnv
+      );
   };
 }
