099c28b18a701474a5b274adce17d7103a7b3457
ggemre <ggemre+github@proton.me>
2025-08-27 21:33:40 +0700

    fix: rm redundant variables

--- modules/programs/hyprland/default.nix
+++ modules/programs/hyprland/default.nix
@@ -22,28 +22,8 @@ in {
 
   config = lib.mkIf cfg.enable {
     environment.variables = {
-      XCURSOR_SIZE = 10;
-      WLR_NO_HARDWARE_CURSORS = 1;
-      WLR_RENDERER_ALLOW_SOFTWARE = 1;
       XDG_CURRENT_DESKTOP = "Hyprland";
-      XDG_SESSION_TYPE = "wayland";
       XDG_SESSION_DESKTOP = "Hyprland";
-
-      # Toolkit backends
-      GDK_BACKEND = "wayland,x11,*";
-      QT_QPA_PLATFORM = "wayland;xcb";
-      SDL_VIDEODRIVER = "wayland";
-      CLUTTER_BACKEND = "wayland";
-      OZONE_PLATFORM = "wayland";
-      ELECTRON_OZONE_PLATFORM_HINT = "wayland";
-
-      # QT specifics
-      QT_AUTO_SCREEN_SCALE_FACTOR = 1;
-      QT_WAYLAND_DISABLE_WINDOWDECORATION = 1;
-
-      # Application specifics
-      MOZ_ENABLE_WAYLAND = 1;
-      NIXOS_OZONE_WL = 1;
     };
 
     programs.hyprland = {
