aee5574366c61f214a4d9448dfeda17c27921210
ggemre <ggemre+github@proton.me>
2025-09-01 12:18:33 +0700

    feat: git aliases

--- modules/config/git/default.nix
+++ modules/config/git/default.nix
@@ -6,9 +6,16 @@ _: {
         email = "ggemre+github@proton.me";
         name = "ggemre";
       };
+
       init.defaultBranch = "main";
       branch.autosetupmerge = "true";
       push.default = "current";
+
+      alias = {
+        s = "status";
+        ls = "log --pretty=format:\"%C(yellow)%h%Cred%d\\\\ %Creset%s%Cblue\\\\ [%cn]\" --decorate";
+        ll = "log --pretty=format:\"%C(yellow)%h%Cred%d\\\\ %Creset%s%Cblue\\\\ [%cn]\" --decorate --numstat";
+      };
     };
   };
 }
