b2572e8dc5d2bfa1347b148710d9f0463c7e5c03
ggemre <ggemre+github@proton.me>
2025-09-10 14:44:50 +0700

    feat: wmenu flags config

--- modules/profiles/desktop/amethyst/wmenu/default.nix
+++ modules/profiles/desktop/amethyst/wmenu/default.nix
@@ -1,6 +1,19 @@
-{self, ...}: {
+{
+  self,
+  config,
+  ...
+}: {
   imports = [
     self.nixosModules.programs.wmenu
   ];
-  programs.wmenu.enable = true;
+  programs.wmenu = {
+    enable = true;
+    flags = ''
+      -bi -l 4 \
+      -N ${config.theme.colors.base00}D0 \
+      -n ${config.theme.colors.base05} \
+      -S ${config.theme.colors.base0D}D0 \
+      -s ${config.theme.colors.base00}
+    '';
+  };
 }
--- modules/programs/wmenu/default.nix
+++ modules/programs/wmenu/default.nix
@@ -12,14 +12,22 @@
     preferLocalBuild = true;
     nativeBuildInputs = [ pkgs.makeWrapper ];
     postBuild = ''
-      wrapProgram $out/bin/wmenu-run \
-        --add-flags "-bi -l 4 -N ${config.theme.colors.base00}D0 -n ${config.theme.colors.base05} -S ${config.theme.colors.base0D}D0 -s ${config.theme.colors.base00}"
+      ${lib.optionalString (cfg.flags != null) ''
+        wrapProgram $out/bin/wmenu-run --add-flags "${cfg.flags}"
+      ''}
     '';
   };
 in {
   options.programs.wmenu = {
     enable = lib.mkEnableOption "Whether to enable the wmenu launcher.";
     package = lib.mkPackageOption pkgs "wmenu" {};
+
+    flags = lib.mkOption {
+      type = lib.types.nullOr lib.types.str;
+      default = null;
+      description = "Custom flags to pass to wmenu when wrapping it.";
+    };
+
     runner = lib.mkOption {
       type = lib.types.str;
       readOnly = true;
