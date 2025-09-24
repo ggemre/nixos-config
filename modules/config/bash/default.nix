e1b28e3e5cf9d301843891f3d2dd6e532adc622c
ggemre <ggemre+github@proton.me>
2025-09-24 12:56:55 +0700

    feat: modern aliases

--- modules/config/bash/default.nix
+++ modules/config/bash/default.nix
@@ -1,6 +1,15 @@
-_: {
+{
+  pkgs,
+  lib,
+  ...
+}: {
   programs.bash = {
     enable = true;
     vteIntegration = true;
+    shellAliases = {
+      ls = lib.getExe pkgs.eza;
+      cat = lib.getExe pkgs.bat;
+      grep = lib.getExe pkgs.ripgrep;
+    };
   };
 }
