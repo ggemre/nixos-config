370717d7d348a1a34d6735d9dccf739798cafac4
ggemre <ggemre+github@proton.me>
2026-03-01 15:52:42 -0500

    fix: rm outdated comment

--- modules/desktop/amethyst/getty/default.nix
+++ modules/desktop/amethyst/getty/default.nix
@@ -4,7 +4,6 @@
   ...
 }: let
   # Find the first "normal" user for autologin.
-  # This is no problem because we only use autologin on our laptops which have just one user.
   normalUsers =
     lib.filterAttrs (_: u: u.isNormalUser or false) config.users.users;
   firstUser =
