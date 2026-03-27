ab3601ab46cbbea2622a50e432988ea2dce5dadc
ggemre <ggemre+github@proton.me>
2026-03-27 10:52:16 -0400

    feat: better trackpad settings

--- modules/desktop/topaz/mangowm/default.nix
+++ modules/desktop/topaz/mangowm/default.nix
@@ -17,10 +17,14 @@
     settings = {
       no_border_when_single = false;
       focus_on_activate = true;
+
       mouse_natural_scrolling = false;
       disable_trackpad = false;
       tap_to_click = false;
+      click_method = 2; # 1 finger left click, 2 finger right click
+
       scroller_prefer_overspread = true;
+      edge_scroller_pointer_focus = false;
 
       exec-once = [
         (lib.getExe config.programs.waybar.package)
