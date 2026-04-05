a2e4b999492a12980cc4583f6c66dcea14c306b9
ggemre <ggemre+github@proton.me>
2026-04-05 19:22:38 -0400

    feat: auxpkgs cache

--- modules/common/nix/default.nix
+++ modules/common/nix/default.nix
@@ -21,10 +21,12 @@
 
       substituters = [
         "https://nix-community.cachix.org"
+        "https://ggemre.cachix.org"
         # "https://nixpkgs-unfree.cachix.org"
       ];
       trusted-public-keys = [
         "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+        "ggemre.cachix.org-1:ULcTF3sXFvs42La9WBB+hUOLuI3eFExxQBpRMgOzTdo="
         # "nixpkgs-unfree.cachix.org-1:hqvoInulhbV4nJ9yJOEr+4wxhDV4xq2d1DK7S6Nj6rs="
       ];
 
