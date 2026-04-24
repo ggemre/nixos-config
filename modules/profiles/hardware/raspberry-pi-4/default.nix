fa92772c2118ef710300ee494f94a8ea0c6d7981
ggemre <80432807+ggemre@users.noreply.github.com>
2026-04-24 15:19:10 -0400

    feat: mainline kernel all hosts

--- modules/profiles/hardware/raspberry-pi-4/default.nix
+++ modules/profiles/hardware/raspberry-pi-4/default.nix
@@ -1,8 +1,6 @@
 # Cherry-picked options from https://github.com/NixOS/nixos-hardware/blob/master/raspberry-pi/4/default.nix
-{pkgs, ...}: {
+_: {
   boot = {
-    kernelPackages = pkgs.linuxPackages_rpi4;
-
     initrd = {
       availableKernelModules = [
         "usb-storage"
