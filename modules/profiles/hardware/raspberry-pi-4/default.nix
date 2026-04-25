f4abe6dbb47078362aa477046318b7981b405d2e
ggemre <80432807+ggemre@users.noreply.github.com>
2026-04-25 19:24:27 -0400

    fix: rpi firmware attempt

--- modules/profiles/hardware/raspberry-pi-4/default.nix
+++ modules/profiles/hardware/raspberry-pi-4/default.nix
@@ -1,5 +1,9 @@
 # Cherry-picked options from https://github.com/NixOS/nixos-hardware/blob/master/raspberry-pi/4/default.nix
 _: {
+  imports = [
+    ./sd-image.nix
+  ];
+
   boot = {
     initrd = {
       availableKernelModules = [
@@ -17,5 +21,6 @@ _: {
   # FIXME: what makes Ethernet work?
   # hardware.deviceTree.filter = "bcm2711-rpi-*.dtb";
   hardware.enableAllHardware = true;
+  hardware.enableAllFirmware = true;
   powerManagement.cpuFreqGovernor = "ondemand";
 }
