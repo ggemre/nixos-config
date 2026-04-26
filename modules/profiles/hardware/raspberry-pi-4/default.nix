db2b4fac7b9f5ab14b2bb2d8f0dc2055854b2016
ggemre <80432807+ggemre@users.noreply.github.com>
2026-04-26 14:29:42 -0400

    fix: revert f4abe6d & e82faee

--- modules/profiles/hardware/raspberry-pi-4/default.nix
+++ modules/profiles/hardware/raspberry-pi-4/default.nix
@@ -1,9 +1,5 @@
 # Cherry-picked options from https://github.com/NixOS/nixos-hardware/blob/master/raspberry-pi/4/default.nix
 _: {
-  imports = [
-    ./sd-image.nix
-  ];
-
   boot = {
     initrd = {
       availableKernelModules = [
@@ -14,13 +10,10 @@ _: {
         "reset-raspberrypi"
         "genet"
       ];
-      # systemd.tpm2.enable = false;
+      systemd.tpm2.enable = false;
     };
   };
 
-  # FIXME: what makes Ethernet work?
-  # hardware.deviceTree.filter = "bcm2711-rpi-*.dtb";
-  hardware.enableAllHardware = true;
-  hardware.enableAllFirmware = true;
+  hardware.deviceTree.filter = "bcm2711-rpi-*.dtb";
   powerManagement.cpuFreqGovernor = "ondemand";
 }
