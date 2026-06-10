02d92ca291994c449bc7636151b786ddb5e76fa4
ggemre <80432807+ggemre@users.noreply.github.com>
2026-06-10 15:07:46 -0400

    feat: stable auxpkgs branch

--- flake.nix
+++ flake.nix
@@ -3,7 +3,7 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-    aux-nixpkgs.url = "github:ggemre/aux-nixpkgs/main";
+    aux-nixpkgs.url = "github:ggemre/aux-nixpkgs/stable";
   };
 
   outputs = inputs @ {
