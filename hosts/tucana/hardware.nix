a4e4f4fcdd5ce8f772ff5f9ef32394b853d7af1f
ggemre <80432807+ggemre@users.noreply.github.com>
2026-06-29 19:51:31 -0400

    nit: formatting

--- hosts/tucana/hardware.nix
+++ hosts/tucana/hardware.nix
@@ -1,22 +1,28 @@
-{ config, lib, modulesPath, ... }:
 {
-  imports =
-    [ (modulesPath + "/installer/scan/not-detected.nix")
-    ];
+  config,
+  lib,
+  modulesPath,
+  ...
+}: {
+  imports = [
+    (modulesPath + "/installer/scan/not-detected.nix")
+  ];
 
-  boot.initrd.availableKernelModules = [ "ahci" "ohci_pci" "ehci_pci" "usb_storage" "sd_mod" "sr_mod" "rtsx_pci_sdmmc" ];
-  boot.initrd.kernelModules = [ ];
-  boot.kernelModules = [ "kvm-amd" ];
-  boot.extraModulePackages = [ ];
+  boot = {
+    initrd.availableKernelModules = [ "ahci" "ohci_pci" "ehci_pci" "usb_storage" "sd_mod" "sr_mod" "rtsx_pci_sdmmc" ];
+    initrd.kernelModules = [];
+    kernelModules = [ "kvm-amd" ];
+    extraModulePackages = [];
+  };
 
-  fileSystems."/" =
-    { device = "/dev/disk/by-partlabel/ROOT";
-      fsType = "ext4";
-    };
+  fileSystems."/" = {
+    device = "/dev/disk/by-partlabel/ROOT";
+    fsType = "ext4";
+  };
 
-  swapDevices =
-    [ { device = "/dev/disk/by-partlabel/SWAP"; }
-    ];
+  swapDevices = [
+    { device = "/dev/disk/by-partlabel/SWAP"; }
+  ];
 
   nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
   hardware.cpu.amd.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
