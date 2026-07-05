11e2d9f88ea041c5264b1876289200d2e73307fa
ggemre <80432807+ggemre@users.noreply.github.com>
2026-07-05 13:25:26 -0400

    feat: tucana only tty

--- hosts/tucana/default.nix
+++ hosts/tucana/default.nix
@@ -6,19 +6,11 @@
 
     # Configure programs & services
     selfModules.config.bash
-    selfModules.config.firefox
-    selfModules.config.foot
     selfModules.config.git
     selfModules.config.helix
-    selfModules.config.starship
     selfModules.config.utils
 
-    # Set a desktop environment
-    selfModules.desktop.garnet
-
     # Include any desired profiles
-    selfModules.profiles.graphical
     selfModules.profiles.hardware.hp-pavilion-g6
-    selfModules.profiles.laptop
   ];
 }
