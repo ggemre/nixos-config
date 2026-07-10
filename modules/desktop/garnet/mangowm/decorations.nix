9c84bbbc4fa087551732ffedab12936aad15141f
ggemre <80432807+ggemre@users.noreply.github.com>
2026-07-10 15:37:19 -0400

    nit: mango color helper function

--- modules/desktop/garnet/mangowm/decorations.nix
+++ modules/desktop/garnet/mangowm/decorations.nix
@@ -2,22 +2,24 @@
   config,
   selfLib,
   ...
-}: {
+}: let
+  color = hexcode: (selfLib.colors.hexa config.theme.colors."base${hexcode}" "FF");
+in {
   programs.mangowm.settings = {
     gappih = 3;
     gappiv = 3;
     gappoh = 3;
     gappov = 3;
 
-    scratchpadcolor = selfLib.colors.hexa config.theme.colors.base0E "FF";
-    rootcolor = selfLib.colors.hexa config.theme.colors.base01 "FF";
+    scratchpadcolor = color "0E";
+    rootcolor = color "01";
 
     borderpx = 2;
     border_radius = 0;
-    bordercolor = selfLib.colors.hexa config.theme.colors.base03 "FF";
-    focuscolor = selfLib.colors.hexa config.theme.colors.base0D "FF";
-    maximizescreencolor = selfLib.colors.hexa config.theme.colors.base03 "FF";
-    globalcolor = selfLib.colors.hexa config.theme.colors.base06 "FF";
+    bordercolor = color "03";
+    focuscolor = color "0D";
+    maximizescreencolor = color "03";
+    globalcolor = color "06";
 
     animations = false;
   };
