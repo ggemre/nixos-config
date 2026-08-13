24ebd68b2a7e3ed72c9d6311ea6e3c44393c4c2f
ggemre <80432807+ggemre@users.noreply.github.com>
2026-08-13 14:28:41 -0400

    fix: string concat bug

--- modules/config/firefox/profiles/main/user-chrome.nix
+++ modules/config/firefox/profiles/main/user-chrome.nix
@@ -1,9 +1,5 @@
 {config, ...}: let
   sharedUserChrome = import ../shared/user-chrome.nix { inherit config; };
 in {
-  programs.firefox.profiles.main.userChrome =
-    sharedUserChrome
-    ++ ''
-
-    '';
+  programs.firefox.profiles.main.userChrome = sharedUserChrome;
 }
--- modules/config/firefox/profiles/youtube/user-chrome.nix
+++ modules/config/firefox/profiles/youtube/user-chrome.nix
@@ -1,9 +1,5 @@
 {config, ...}: let
   sharedUserChrome = import ../shared/user-chrome.nix { inherit config; };
 in {
-  programs.firefox.profiles.youtube.userChrome =
-    sharedUserChrome
-    ++ ''
-
-    '';
+  programs.firefox.profiles.youtube.userChrome = sharedUserChrome;
 }
