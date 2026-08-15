ce2fc4d931f722fd3e40153942b303ddc45b7a1a
ggemre <80432807+ggemre@users.noreply.github.com>
2026-08-15 19:45:03 -0400

    fix: call mangobar from module

--- modules/desktop/garnet/mango/default.nix
+++ modules/desktop/garnet/mango/default.nix
@@ -2,7 +2,6 @@
   config,
   lib,
   pkgs,
-  selfPkgs,
   ...
 }: {
   imports = [
@@ -46,9 +45,9 @@
       ];
 
       exec-once = [
+        (lib.getExe config.programs.mangobar.package)
         (lib.getExe config.services.swayidle.package)
         (lib.getExe pkgs.wayland-pipewire-idle-inhibit)
-        (lib.getExe selfPkgs.mangobar)
       ];
     };
   };
